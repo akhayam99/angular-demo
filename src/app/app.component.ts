8d62082a533d619e2c6284ef88f6e15eb479a55a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-20 18:40:18 -0400

    improve selection with and without ctrl command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { ServiceNameService } from './http-error.service';
 
 export class AppComponent implements AfterViewInit {
   activeMonths: string[] = [];
+  clickCommand: boolean = false;
+  ctrlCommand: boolean = false;
   dataJson$!: Observable<any[]>;
   dataUrl: string = "http://staccah.fattureincloud.it/testfrontend/data.json";
   formatter: Intl.NumberFormat = Intl.NumberFormat('it-IT', {
@@ -39,9 +41,22 @@ export class AppComponent implements AfterViewInit {
 
   constructor(private httpClient: HttpClient, public serviceNameService: ServiceNameService) {
     window.addEventListener('mouseup', e => {
-      if (!e.ctrlKey)
-        this.selectActive = false;
+      this.clickCommand = false;
+      this.selectActive = false;
     });
+
+    window.addEventListener('mousedown', e => {
+      this.clickCommand = true;
+      this.selectActive = true;
+    });
+
+    window.addEventListener('keydown', e => {
+      this.ctrlCommand = true;
+    })
+
+    window.addEventListener('keyup', e => {
+      this.ctrlCommand = false;
+    })
   }
 
   ngAfterViewInit(): void {
@@ -60,9 +75,12 @@ export class AppComponent implements AfterViewInit {
     });
   }
 
-  public startActive(month: string, document: string, amount: string, event: MouseEvent) {
-    if (event.ctrlKey)
-      return this.addToActiveMonths(month, amount, document);
+  public startActive(month: string, document: string, amount: string) {
+    if (this.ctrlCommand) {
+      if (!this.activeMonths.includes(month))
+        return this.addToActiveMonths(month, amount, document);
+      return this.removeFromActiveMonths(month, amount, document);
+    }
 
     this.selectActive = true;
     this.activeMonths = [month];
@@ -70,14 +88,15 @@ export class AppComponent implements AfterViewInit {
     this.totalDocument = parseInt(document);
   }
 
-  public setActive(month: string, document: string, amount: string, event: MouseEvent) {
+  public setActive(month: string, document: string, amount: string) {
     if (!this.selectActive)
       return;
 
+    if (this.selectActive && this.ctrlCommand)
+      if (!this.activeMonths.includes(month))
+        return this.addToActiveMonths(month, amount, document)
+
     if (this.activeMonths.includes(month))
-      return;
-    
-    if (event.ctrlKey)
       return;
 
     var firstId: number = this.months.indexOf(this.activeMonths[0]);
@@ -109,6 +128,12 @@ export class AppComponent implements AfterViewInit {
     this.activeMonths.push(month);
     this.totalAmount += parseInt(amount);
     this.totalDocument += parseInt(document);
+  }
+
+  private removeFromActiveMonths(month: string, amount: string, document: string) {
+    this.activeMonths[this.activeMonths.indexOf(month)] = '';
+    this.totalAmount -= parseInt(amount);
+    this.totalDocument -= parseInt(document);
   }
 
   public getPrice(value: number): string {
